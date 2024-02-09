f39ef8503d5b4452adaa209fdc70f2866f15572b
Michelle Baird <michelle.baird@fishtownanalytics.com>
2024-02-09 20:19:36 +0000

    put order status in dim_customers

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -14,14 +14,14 @@ customer_orders as (
 
     select
         customer_id,
-
+        order_status,
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
-
+       
     from orders
 
-    group by 1
+    group by 1,2
 
 ),
 
@@ -33,7 +33,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.order_status
 
     from customers
 
